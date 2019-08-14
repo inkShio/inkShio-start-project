282a5e9a06691050cc4a78919e087c014076ee07
Ink Shio <ink.shio@gmail.com>
2019-08-15 01:10:11 +0300

    fix: scss task

--- gulp-tasks/scss.js
+++ gulp-tasks/scss.js
@@ -4,9 +4,11 @@ import { paths } from "../gulpfile.babel";
 import gulp from "gulp";
 import sourcemaps from "gulp-sourcemaps";
 import plumber from "gulp-plumber";
+import globbing from "gulp-css-globbing";
 import sass from "gulp-sass";
 import autoprefixer from "gulp-autoprefixer";
 import gcmq from "gulp-group-css-media-queries";
+import csscomb from "gulp-csscomb";
 import mincss from "gulp-clean-css";
 import rename from "gulp-rename";
 import browsersync from "browser-sync";
@@ -18,12 +20,20 @@ gulp.task("scss", () => {
     .pipe(sourcemaps.init())
     // Вывод ошибки
     .pipe(plumber())
+    // Для поиска всех файлов sass (scss)
+		.pipe(globbing({
+			extensions: ['.scss']
+		}))
     // Используем scss
-    .pipe(sass())
+    .pipe(sass({
+			outputStyle: 'expanded'
+		}))
     // Ставим префиксы
     .pipe(autoprefixer())
     // Группируем медиа запросы
     .pipe(gcmq())
+    // Упорядочим стили
+		.pipe(csscomb())
     // Минифицируем css
     .pipe(mincss())
     // Добавляем к названию min
